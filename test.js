d5d22302fb686a408ed405715eefd44636a3ba51
jjg62487 <jjg62487@gmail.com>
2015-02-22 20:23:42 -0500

    Added html and js file
    added the html file and the js file

---
+++ test.js
@@ -0,0 +1,14 @@
+document.write("hello world!! <br>");
+document.write("is this correct <br>");
+document.write("whats up?<br>");
+document.write("hey hey hey hey<br>");
+document.write("hellow<br>");
+document.write("nice try<br>")
+document.write("what<br>");
+
+if(4>2){
+	document.write("statement is true");
+}
+else{
+	document("statement is false");
+}
